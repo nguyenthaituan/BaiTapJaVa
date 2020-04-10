e65e27fb7e3fc0bb964d0089c019bb74545a758b
Admin <Admin@192.168.193.2>
2020-04-11 00:06:10 +0700

    NguyenThaiTuan_59132942_BaiTapJava

--- src/main/java/BaiTap3/SinhVienBiz.java
+++ src/main/java/BaiTap3/SinhVienBiz.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package BaiTap3;
 
-/**
- *
- * @author Admin
- */
 public class SinhVienBiz extends SinhVienPoLy{
     private double diemMaketing;
     private double diemSales;
